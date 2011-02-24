850184f2da39faebae379f12a602c0e10c35a285
Todd Eddy <vr@vrillusions.com>
2011-02-24 13:08:01 -0500

    Add a simple web server script.

---
+++ web-server/simplehttpserver.py
@@ -0,0 +1,16 @@
+# File: simplehttpserver-example-1.py
+
+import SimpleHTTPServer
+import SocketServer
+
+# minimal web server.  serves files relative to the
+# current directory.
+
+PORT = 8000
+
+Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+
+httpd = SocketServer.TCPServer(("", PORT), Handler)
+
+print "serving at port", PORT
+httpd.serve_forever()
