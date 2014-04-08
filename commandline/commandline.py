e7548981f1874dcb47c2cecba1b0658154122fd9
Todd Eddy <vr@vrillusions.com>
2014-04-08 01:53:30 -0400

    commandline: Show how to hide commands from help output

--- commandline/commandline.py
+++ commandline/commandline.py
@@ -18,6 +18,17 @@ class MyApp(cmd.Cmd):
     intro = "Type 'help' for a list of commands"
     #prompt = 'cmd> '
 
+    def get_names(self):
+        """Filter out commands not to show in help.
+
+        Note: while 'help' doesn't show you can still do a 'help EOF' and it
+        will show the docstring for do_EOF().
+
+        """
+        ignored = ('do_EOF', 'do_admin')
+        names = [x for x in dir(self.__class__) if x not in ignored]
+        return names
+
     def emptyline(self):
         """Called when user hits enter again without typing anything.
 
@@ -26,15 +37,20 @@ class MyApp(cmd.Cmd):
         """
         pass
 
-    def do_hello(self, line):
-        """Hello..."""
-        print("world")
+    def do_admin(self, line):
+        """Super secret command."""
+        # this is filtered, see get_names()
+        print('My password is: correct horse battery staple')
 
     def do_EOF(self, line):
         """Type ctrl-d or 'quit' to exit."""
         # Add print to insert line break at end
         print()
         return True
+
+    def do_hello(self, line):
+        """Hello..."""
+        print("world")
 
     def do_quit(self, line):
         """Quit"""
